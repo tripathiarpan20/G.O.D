0426128eb2ea1e702701dfbb04cc9d6693584060
namoray <neuvaamor@proton.me>
2024-12-12 15:03:26 +0100

    13

--- validator/db/sql/tasks.py
+++ validator/db/sql/tasks.py
@@ -23,7 +23,7 @@ async def add_task(task: RawTask, psql_db: PSQLDB) -> RawTask:
             {cst.FIELD_INSTRUCTION}, {cst.FIELD_INPUT}, {cst.STATUS},
              {cst.HOURS_TO_COMPLETE}, {cst.FIELD_OUTPUT}, {cst.FORMAT},
              {cst.NO_INPUT_FORMAT}, {cst.IS_ORGANIC}, {cst.CREATED_AT})
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
             RETURNING {cst.TASK_ID}
         """
         task_id = await connection.fetchval(
