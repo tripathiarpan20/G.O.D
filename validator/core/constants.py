86ccb61800d3306b7506a2a96ac30d00956d1b8e
WanderingWeights <wanderingweights1@gmail.com>
2024-12-12 08:14:56 -0600

    slight drop in minimal score needed (#98)

--- validator/core/constants.py
+++ validator/core/constants.py
@@ -78,7 +78,7 @@ TEST_SCORE_WEIGHTING = 0.7  # synth will be (1 - this)
 TARGET_SCORE_RATIO = 1
 MIN_TASK_SCORE = -0.02  # very tiny punishment while miners find their feet
 MAX_TASK_SCORE = 1.6
-TASK_SCORE_THRESHOLD = 0.95
+TASK_SCORE_THRESHOLD = 0.9
 REWEIGHTING_EXP = 0.7  # how much of a drop off from leader
 SCORING_WINDOW = 7  # number of days over which we score
 
