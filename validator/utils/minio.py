6190fdb371c0a9f5b7450c82770b6b0aadb18e74
namoray <neuvaamor@proton.me>
2024-12-14 18:41:05 +0100

    less loops

--- validator/utils/minio.py
+++ validator/utils/minio.py
@@ -21,7 +21,6 @@ class AsyncMinioClient:
             secure=secure,
             region=region,
         )
-        self.loop = asyncio.get_event_loop()
         self.executor = ThreadPoolExecutor()
 
     async def upload_file(self, bucket_name, object_name, file_path):
@@ -29,41 +28,46 @@ class AsyncMinioClient:
         args = (bucket_name, object_name, file_path)
         logger.info("Attempting to upload")
         try:
-            result = await self.loop.run_in_executor(self.executor, func, *args)
+            loop = asyncio.get_event_loop()
+            result = await loop.run_in_executor(self.executor, func, *args)
             logger.info(
                 f"The bucket_name is {bucket_name} and the result was {result}")
             return result
         except Exception as e:
-            logger.info(f"There was an issue with uploading {e}")
+            logger.info(f"There was an issue with uploading file to s3. {e}")
             return False
 
     async def download_file(self, bucket_name, object_name, file_path):
         func = self.client.fget_object
         args = (bucket_name, object_name, file_path)
         logger.info("Attempting to download")
-        return await self.loop.run_in_executor(self.executor, func, *args)
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(self.executor, func, *args)
 
     async def delete_file(self, bucket_name, object_name):
         func = self.client.remove_object
         args = (bucket_name, object_name)
-        return await self.loop.run_in_executor(self.executor, func, *args)
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(self.executor, func, *args)
 
     async def list_objects(self, bucket_name, prefix=None, recursive=True):
         func = self.client.list_objects
         args = (bucket_name, prefix, recursive)
         logger.info("Listing objects")
-        return await self.loop.run_in_executor(self.executor, func, *args)
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(self.executor, func, *args)
 
     async def ensure_bucket_exists(self, bucket_name):
-        exists = await self.loop.run_in_executor(self.executor, self.client.bucket_exists, bucket_name)
+        exists = await self.client.bucket_exists(bucket_name)
         if not exists:
-            await self.loop.run_in_executor(self.executor, self.client.make_bucket, bucket_name)
+            await self.client.make_bucket(bucket_name)
 
     async def get_presigned_url(self, bucket_name, object_name, expires=604800):
         expires_duration = datetime.timedelta(seconds=expires)
         func = self.client.presigned_get_object
         args = (bucket_name, object_name, expires_duration)
-        return await self.loop.run_in_executor(self.executor, func, *args)
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(self.executor, func, *args)
 
     def get_public_url(self, bucket_name, object_name):
         return f"https://{self.endpoint}/{bucket_name}/{object_name}"
