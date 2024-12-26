6d5514f4db53c23715b0c055fdecd44115fb60ef
WanderingWeights <wanderingweights1@gmail.com>
2024-12-26 15:42:24 -0600

    More emphasis on winners

--- validator/core/constants.py
+++ validator/core/constants.py
@@ -91,7 +91,7 @@ TARGET_SCORE_RATIO = 1.05
 MIN_TASK_SCORE = 0.0  # very tiny punishment while miners find their feet
 MAX_TASK_SCORE = 1.6
 TASK_SCORE_THRESHOLD = 0.9
-REWEIGHTING_EXP = 0.4  # how much of a drop off from leader
+REWEIGHTING_EXP = 0.6  # how much of a drop off from leader
 SCORING_WINDOW = 7  # number of days over which we score
 
 # processing stuff
