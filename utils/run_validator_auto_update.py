f4eb190d6c321c6829fddd74a00d9219e1972ec2
WanderingWeights <wanderingweights1@gmail.com>
2024-12-27 16:51:01 -0600

    Fix (#164)
    Pulling latest vali docker image

--- utils/run_validator_auto_update.py
+++ utils/run_validator_auto_update.py
@@ -3,55 +3,47 @@ import os
 import subprocess
 import time
 
+from core.constants import VALIDATOR_DOCKER_IMAGE
 
-def should_update_local(local_commit, remote_commit):
+
+def pull_latest_docker_images():
+    os.system(f"docker pull {VALIDATOR_DOCKER_IMAGE}")
+
+
+def should_update_local(local_commit: str, remote_commit: str) -> bool:
     return local_commit != remote_commit
 
 
 def run_auto_updater():
-    print("Starting auto-updater...")
-    print("Checking for .vali.env file...")
     while not os.path.exists(".vali.env"):
-        print(".vali.env file not found. Waiting 10 seconds before checking again...")
         time.sleep(10)
-    print(".vali.env file found. Proceeding with docker setup...")
+
+    pull_latest_docker_images()
+
     launch_command = "task validator"
     os.system(launch_command)
     time.sleep(60)
 
     while True:
-        print("Checking for updates...")
         current_branch = subprocess.getoutput("git rev-parse --abbrev-ref HEAD")
         local_commit = subprocess.getoutput("git rev-parse HEAD")
         os.system("git fetch")
         remote_commit = subprocess.getoutput(f"git rev-parse origin/{current_branch}")
 
         if should_update_local(local_commit, remote_commit):
-            print("Local repo is not up-to-date. Updating...")
             reset_cmd = f"git reset --hard {remote_commit}"
             process = subprocess.Popen(reset_cmd.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
-
-            if error:
-                print("Error in updating:", error)
-            else:
-                print("Updated local repo to latest version:", remote_commit)
-
-                print("Running the autoupdate steps...")
+            if not error:
+                pull_latest_docker_images()
                 os.system("./utils/autoupdate_validator_steps.sh")
                 time.sleep(20)
 
-                print("Finished running the autoupdate steps! Ready to go 😎")
-
-        else:
-            print("Repo is up-to-date.")
-
+        pull_latest_docker_images()
         time.sleep(10)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run auto updates for a validator")
-
     args = parser.parse_args()
-
     run_auto_updater()
