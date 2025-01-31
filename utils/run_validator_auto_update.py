c5d3506082981cca79f058b419e0620d3743fc92
samoline1 <wolveinstein19@gmail.com>
2025-01-31 17:33:45 +0100

    rebuild up-to-date docker images (#260)

--- utils/run_validator_auto_update.py
+++ utils/run_validator_auto_update.py
@@ -4,8 +4,14 @@ import subprocess
 import time
 
 
-def pull_latest_docker_images():
-    os.system("docker pull weightswandering/tuning_vali:latest")
+def rebuild_docker_images():
+    os.system(
+        "docker build -f dockerfiles/validator.dockerfile -t weightswandering/tuning_vali:latest ."
+    )
+    os.system(
+        "docker build -f dockerfiles/validator-diffusion.dockerfile -t diagonalge/tuning_validator_diffusion:latest ."
+    )
+
 
 
 def should_update_local(local_commit: str, remote_commit: str) -> bool:
@@ -16,7 +22,7 @@ def run_auto_updater():
     while not os.path.exists(".vali.env"):
         time.sleep(10)
 
-    pull_latest_docker_images()
+    rebuild_docker_images()
 
     launch_command = "task validator"
     os.system(launch_command)
@@ -33,7 +39,7 @@ def run_auto_updater():
             process = subprocess.Popen(reset_cmd.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
             if not error:
-                pull_latest_docker_images()
+                rebuild_docker_images()
                 os.system("./utils/autoupdate_validator_steps.sh")
                 time.sleep(20)
 
