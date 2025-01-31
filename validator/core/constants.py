5025073b91cadaa70de427cc50ff7dd5681a074b
namoray <152790419+namoray@users.noreply.github.com>
2025-01-31 16:42:13 +0000

    increase task score threshold (#267)
    Ya gotta do good boi

--- validator/core/constants.py
+++ validator/core/constants.py
@@ -94,7 +94,7 @@ TEST_SCORE_WEIGHTING = 0.7  # synth will be (1 - this)
 TARGET_SCORE_RATIO = 1.5
 MIN_TASK_SCORE = -0.05  # very tiny punishment while miners find their feet
 MAX_TASK_SCORE = 1.8
-TASK_SCORE_THRESHOLD = 0.85
+TASK_SCORE_THRESHOLD = 0.95
 REWEIGHTING_EXP = 0.7  # how much of a drop off from leader
 
 SCORING_WINDOW = 7  # number of days over which we score
