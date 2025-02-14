9aeab9bf84601cc3b8b009f5f9baf5096d9c208d
namoray <152790419+namoray@users.noreply.github.com>
2025-02-14 00:10:16 +0000

    Install issues (#296)

--- utils/autoupdate_validator_steps.sh
+++ utils/autoupdate_validator_steps.sh
@@ -1,5 +1,11 @@
 # Steps to take to update the validator automatically
 # Change each time but take caution
 
-task install
+deactivate || true
+rm -rf $HOME/.venv
+python -m venv $HOME/.venv
+chown -R $SUDO_USER:$SUDO_USER $HOME/.venv $HOME/.bashrc
+source $HOME/.venv/bin/activate
+pip install -e .
+
 task validator
