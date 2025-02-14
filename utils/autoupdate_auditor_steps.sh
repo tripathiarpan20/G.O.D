debf9c43f15f53dc62a1f502cf9434a6b60c3bc6
namoray <152790419+namoray@users.noreply.github.com>
2025-02-14 00:22:20 +0000

    dont del everytime (#297)

--- utils/autoupdate_auditor_steps.sh
+++ utils/autoupdate_auditor_steps.sh
@@ -1,10 +1,6 @@
 # Steps to take to update the auditor automatically
 # Change each time but take caution
 
-deactivate || true
-rm -rf $HOME/.venv
-python -m venv $HOME/.venv
-chown -R $SUDO_USER:$SUDO_USER $HOME/.venv $HOME/.bashrc
 source $HOME/.venv/bin/activate
 pip install -e .
 
--- utils/autoupdate_validator_steps.sh
+++ utils/autoupdate_validator_steps.sh
@@ -1,10 +1,7 @@
 # Steps to take to update the validator automatically
 # Change each time but take caution
 
-deactivate || true
-rm -rf $HOME/.venv
-python -m venv $HOME/.venv
-chown -R $SUDO_USER:$SUDO_USER $HOME/.venv $HOME/.bashrc
+
 source $HOME/.venv/bin/activate
 pip install -e .
 
